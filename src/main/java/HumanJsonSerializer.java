e82cde5da0afd4d60622c516c68c6e8708f996ee
kelvin-de-oliveira <kelvin.oliveira.0110@gmail.com>
2026-08-25 17:04:07 -0300

    feat: adiciona HumanJsonSerializer para serializacao e desserializacao JSON com Gson

---
+++ src/main/java/HumanJsonSerializer.java
@@ -0,0 +1,34 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class HumanJsonSerializer {
+
+    private Gson gson;
+
+    public HumanJsonSerializer() {
+        gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd")
+                .setPrettyPrinting()
+                .create();
+    }
+
+    public String toJson(Human human) {
+        return gson.toJson(human);
+    }
+
+    public String toJson(List<Human> humans) {
+        return gson.toJson(humans);
+    }
+
+    public Human fromJson(String json) {
+        return gson.fromJson(json, Human.class);
+    }
+
+    public List<Human> listFromJson(String json) {
+        Type listType = new TypeToken<List<Human>>(){}.getType();
+        return gson.fromJson(json, listType);
+    }
+}
