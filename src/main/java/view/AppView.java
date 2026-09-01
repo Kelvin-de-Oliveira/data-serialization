0c95c749ff5d7e1ac7b5501307873380aeb59ce5
Kelvin de Oliveira <kelvin-de-oliveira>
2026-09-01 11:31:22 -0300

    feat: adiciona AppView para carregar e exibir a interface grafica JavaFX

---
+++ src/main/java/view/AppView.java
@@ -0,0 +1,37 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.net.URL;
+
+public class AppView extends Application {
+
+    private FXMLLoader loader;
+
+    public AppView() {
+        this.loader = new FXMLLoader();
+        URL url = getClass().getResource("/view/app.fxml");
+        this.loader.setLocation(url);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane = loader.load();
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Cadastro de Human");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        System.exit(0);
+    }
+
+    public void run(String[] args) {
+        Application.launch(args);
+    }
+}
