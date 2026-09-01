1cb323decc56f24e08e017e9cd4ee7eb712e23bc
Kelvin de Oliveira <kelvin-de-oliveira>
2026-09-01 11:17:45 -0300

    feat: adiciona HumanFX com propriedades JavaFX observaveis para exibicao na TableView

---
+++ src/main/java/view/HumanFX.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class HumanFX {
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty fullName;
+    private SimpleDoubleProperty income;
+    private SimpleStringProperty birthday;
+
+    public HumanFX(int id, String fullName, double income, String birthday) {
+        this.id = new SimpleIntegerProperty(id);
+        this.fullName = new SimpleStringProperty(fullName);
+        this.income = new SimpleDoubleProperty(income);
+        this.birthday = new SimpleStringProperty(birthday);
+    }
+
+    public int getId() { return id.get(); }
+    public void setId(int id) { this.id.set(id); }
+
+    public String getFullName() { return fullName.get(); }
+    public void setFullName(String fullName) { this.fullName.set(fullName); }
+
+    public double getIncome() { return income.get(); }
+    public void setIncome(double income) { this.income.set(income); }
+
+    public String getBirthday() { return birthday.get(); }
+    public void setBirthday(String birthday) { this.birthday.set(birthday); }
+}
